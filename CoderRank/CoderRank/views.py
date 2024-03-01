644afb2683c54ecc2030fc25e4ff1d826c0b330a
SheejaLawrance <sheejalawrance336@gmail.com>
2024-02-29 23:14:49 -0200

    helloview test

---
+++ CoderRank/CoderRank/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+
+def helloView(request):
+    return HttpResponse("helloView")
