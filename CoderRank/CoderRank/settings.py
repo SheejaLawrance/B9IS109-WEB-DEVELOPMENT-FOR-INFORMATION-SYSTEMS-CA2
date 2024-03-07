06138f57b1ed1f10646e327d14d34daa47928766
SheejaLawrance <sheejalawrance336@gmail.com>
2024-03-07 00:03:11 +0000

    created components and added static folder

--- CoderRank/CoderRank/settings.py
+++ CoderRank/CoderRank/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'CoderRank.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates','static'],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,10 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR=[
-    os.path.join(BASE_DIR,"static")
+STATIC_URL = 'static/' 
+STATIC_ROOT=BASE_DIR/'staticfiles'  #prod
+# STATICFILES_DIR=[
+#     BASE_DIR/'static'   #location
+# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
